53c9d9d8a18b0e0f4f8813f57e5108b965971df1
junpei ogawa <cornerstone.am3@gmail.com>
2025-05-28 19:02:36 +0900

    setup font

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,50 @@
+import {
+  NotoSansJP_100Thin,
+  NotoSansJP_200ExtraLight,
+  NotoSansJP_300Light,
+  NotoSansJP_400Regular,
+  NotoSansJP_500Medium,
+  NotoSansJP_600SemiBold,
+  NotoSansJP_700Bold,
+  NotoSansJP_800ExtraBold,
+  NotoSansJP_900Black,
+  useFonts,
+} from "@expo-google-fonts/noto-sans-jp";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function Index() {
+  const [loaded, error] = useFonts({
+    NotoSansJP_100: NotoSansJP_100Thin,
+    NotoSansJP_200: NotoSansJP_200ExtraLight,
+    NotoSansJP_300: NotoSansJP_300Light,
+    NotoSansJP_400: NotoSansJP_400Regular,
+    NotoSansJP_500: NotoSansJP_500Medium,
+    NotoSansJP_600: NotoSansJP_600SemiBold,
+    NotoSansJP_700: NotoSansJP_700Bold,
+    NotoSansJP_800: NotoSansJP_800ExtraBold,
+    NotoSansJP_900: NotoSansJP_900Black,
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  useEffect(() => {
+    if (error) {
+      console.error("[Error] failed to load fonts: ", error);
+    }
+  }, [error]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View
       style={{
@@ -9,7 +53,13 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text
+        style={{
+          fontFamily: "NotoSansJP_400",
+        }}
+      >
+        Edit app/index.tsx to edit this screen1.
+      </Text>
     </View>
   );
 }
