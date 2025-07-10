72fe5de26de5920b04e2e4f45937cf67e759d7cd
junpei ogawa <cornerstone.am3@gmail.com>
2025-07-10 22:35:13 +0900

    improve Button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,6 +1,6 @@
 import { FC, useMemo } from "react"
 import {
-  StyleSheet,
+  StyleProp,
   TextStyle,
   TouchableOpacity,
   TouchableOpacityProps,
@@ -10,29 +10,107 @@ import {
 import { Typography } from "@/components/atoms/Typography"
 import { createStyleSheet } from "@/styles/theme"
 
+const VARIANT = {
+  primary: "primary",
+  secondary: "secondary",
+} as const
+
+type Variant = (typeof VARIANT)[keyof typeof VARIANT]
+
+const SIZE = {
+  small: "small",
+  medium: "medium",
+} as const
+
+type Size = (typeof SIZE)[keyof typeof SIZE]
+
 interface Props {
+  variant?: Variant
+  size?: Size
   label: string
   containerStyle?: TouchableOpacityProps["style"]
+  labelStyle?: TextStyle
   onPress: TouchableOpacityProps["onPress"]
 }
 
 export const Button: FC<Props> = ({
+  variant = VARIANT.primary,
+  size = SIZE.medium,
   label,
   containerStyle: overrideContainerStyle,
+  labelStyle: overrideLabelStyle,
   onPress,
 }) => {
   const styles = useStyles()
 
   const containerStyle = useMemo(() => {
-    return StyleSheet.compose<ViewStyle, ViewStyle, ViewStyle>(
-      styles.container,
-      overrideContainerStyle,
-    )
-  }, [overrideContainerStyle, styles.container])
+    const stylesPool: StyleProp<ViewStyle> = [styles.buttonBase]
+
+    switch (variant) {
+      case VARIANT.primary:
+        stylesPool.push(styles.primaryButton)
+        break
+      case VARIANT.secondary:
+        stylesPool.push(styles.secondaryButton)
+        break
+    }
+
+    switch (size) {
+      case SIZE.medium:
+        stylesPool.push(styles.mediumButtonContainer)
+        break
+      case SIZE.small:
+        stylesPool.push(styles.smallButtonContainer)
+        break
+    }
+
+    return [...stylesPool, overrideContainerStyle]
+  }, [
+    overrideContainerStyle,
+    size,
+    styles.buttonBase,
+    styles.mediumButtonContainer,
+    styles.primaryButton,
+    styles.secondaryButton,
+    styles.smallButtonContainer,
+    variant,
+  ])
+
+  const labelStyle = useMemo(() => {
+    const stylesPool: StyleProp<TextStyle> = []
+
+    switch (variant) {
+      case VARIANT.primary:
+        stylesPool.push(styles.primaryButtonLabel)
+        break
+      case VARIANT.secondary:
+        stylesPool.push(styles.secondaryButtonLabel)
+        break
+    }
+
+    switch (size) {
+      case SIZE.medium:
+        stylesPool.push(styles.mediumButtonLabel)
+        break
+      case SIZE.small:
+        stylesPool.push(styles.smallButtonLabel)
+        break
+    }
+
+    return [...stylesPool, overrideLabelStyle]
+  }, [
+    overrideLabelStyle,
+    size,
+    styles.mediumButtonLabel,
+    styles.primaryButtonLabel,
+    styles.secondaryButtonLabel,
+    styles.smallButtonLabel,
+    variant,
+  ])
 
   return (
     <TouchableOpacity style={containerStyle} onPress={onPress}>
-      <Typography bold style={styles.label}>
+      <Typography bold style={labelStyle}>
         {label}
       </Typography>
     </TouchableOpacity>
@@ -40,16 +118,40 @@ export const Button: FC<Props> = ({
 }
 
 const useStyles = createStyleSheet((theme) => ({
-  container: {
+  buttonBase: {
     alignItems: "center",
-    backgroundColor: theme.colors.primary[500],
-    borderRadius: theme.borderRadius.md,
     justifyContent: "center",
+  } satisfies ViewStyle,
+  primaryButton: {
+    backgroundColor: theme.colors.primary[500],
+  } satisfies ViewStyle,
+  primaryButtonLabel: {
+    color: theme.colors.text.inverse,
+  } satisfies TextStyle,
+  secondaryButton: {
+    backgroundColor: theme.colors.neutral[200],
+  } satisfies ViewStyle,
+  secondaryButtonLabel: {
+    color: theme.colors.text.secondary,
+  } satisfies TextStyle,
+  mediumButtonContainer: {
+    borderRadius: theme.borderRadius.md,
     minHeight: 45,
+    minWidth: 120,
     paddingHorizontal: theme.spacing[3],
     paddingVertical: theme.spacing[1],
   } satisfies ViewStyle,
-  label: {
-    color: theme.colors.text.inverse,
+  smallButtonContainer: {
+    borderRadius: theme.borderRadius.sm,
+    minHeight: 30,
+    minWidth: 60,
+    paddingHorizontal: theme.spacing[3],
+    paddingVertical: theme.spacing[1],
+  } satisfies ViewStyle,
+  mediumButtonLabel: {
+    fontSize: theme.fontSize.sm,
+  } satisfies TextStyle,
+  smallButtonLabel: {
+    fontSize: theme.fontSize.xs,
   } satisfies TextStyle,
 }))
