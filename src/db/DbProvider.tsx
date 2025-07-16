0b623da6d0ee5b9d462435e748f8d76d1b8ac04f
junpei ogawa <cornerstone.am3@gmail.com>
2025-07-16 14:17:55 +0900

    setup expo-drizzle-studio-plugin

--- src/db/DbProvider.tsx
+++ src/db/DbProvider.tsx
@@ -2,13 +2,15 @@ import { useMigrations } from "drizzle-orm/expo-sqlite/migrator"
 import { FC, ReactNode, useEffect } from "react"
 
 import migrations from "../../drizzle/migrations"
-import { DbContext, db } from "./db"
+import { DbContext, db, useDbViewer } from "./db"
 
 interface Props {
   children: ReactNode
 }
 
 export const DbProvider: FC<Props> = ({ children }) => {
+  useDbViewer()
+
   const { success, error } = useMigrations(db, migrations)
 
   useEffect(() => {
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/expo-sqlite"
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin"
 import * as SQLite from "expo-sqlite"
 import { createContext, useContext } from "react"
 
@@ -9,3 +10,7 @@ type Db = typeof db
 
 export const DbContext = createContext<Db | null>(null)
 export const useDb = (): Db | null => useContext(DbContext)
+
+export const useDbViewer = () => {
+  useDrizzleStudio(expo)
+}
