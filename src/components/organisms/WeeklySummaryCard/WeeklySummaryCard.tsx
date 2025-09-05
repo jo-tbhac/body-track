352d5a62d983cb636bbbe2b8e83efc0e9fe536f9
junpei ogawa <cornerstone.am3@gmail.com>
2025-09-05 14:03:36 +0900

    display weekly summary data

--- src/components/organisms/WeeklySummaryCard/WeeklySummaryCard.tsx
+++ src/components/organisms/WeeklySummaryCard/WeeklySummaryCard.tsx
@@ -1,13 +1,58 @@
-import { FC } from "react"
+import { FC, useMemo } from "react"
 import { TextStyle, View, ViewStyle } from "react-native"
 
 import { Card } from "@/components/atoms/Card"
 import { Typography } from "@/components/atoms/Typography"
 import { SummaryItem } from "@/components/molecules/SummaryItem"
+import { useFindProfile } from "@/hooks/profile/useFindProfile"
+import {
+  SearchWeightRecordsParams,
+  useSearchWeightRecords,
+} from "@/hooks/weightRecord/useSearchWeightRecords"
+import { subDuration } from "@/lib/date"
 import { createStyleSheet } from "@/styles/theme"
+import { calcAverageBmi } from "@/usecase/calcAverageBmi"
+import { calcAverageBodyFatRate } from "@/usecase/calcAverageBodyFatRate"
+import { calcAverageWeight } from "@/usecase/calcAverageWeight"
 
 export const WeeklySummaryCard: FC = () => {
   const styles = useStyles()
+
+  const searchParams: SearchWeightRecordsParams = useMemo(() => {
+    const today = new Date()
+    return {
+      measuredDateFrom: subDuration(today, { weeks: 1 }),
+      measuredDateTo: today,
+    }
+  }, [])
+
+  const [profile] = useFindProfile()
+  const [weightRecords] = useSearchWeightRecords(searchParams)
+
+  const averageWeight = useMemo(() => {
+    if (weightRecords == null) {
+      return null
+    }
+    return calcAverageWeight(weightRecords)
+  }, [weightRecords])
+
+  const averageBodyFatRate = useMemo(() => {
+    if (weightRecords == null) {
+      return null
+    }
+    return calcAverageBodyFatRate(weightRecords)
+  }, [weightRecords])
+
+  const averageBmi = useMemo(() => {
+    if (weightRecords == null || profile == null) {
+      return null
+    }
+    return calcAverageBmi(profile.height, weightRecords)
+  }, [weightRecords, profile])
+
+  const averageWeightLabel = averageWeight?.toFixed(1) ?? "--"
+  const averageBodyFatRateLabel = averageBodyFatRate?.toFixed(1) ?? "--"
+  const averageBmiLabel = averageBmi?.toFixed(1) ?? "--"
 
   return (
     <Card>
@@ -17,9 +62,13 @@ export const WeeklySummaryCard: FC = () => {
         </Typography>
       </View>
       <View style={styles.cardBottom}>
-        <SummaryItem label="平均体重" value={63.8} unit="kg" />
-        <SummaryItem label="平均体脂肪率" value={16.0} unit="%" />
-        <SummaryItem label="平均BMI" value={19.0} />
+        <SummaryItem label="平均体重" value={averageWeightLabel} unit="kg" />
+        <SummaryItem
+          label="平均体脂肪率"
+          value={averageBodyFatRateLabel}
+          unit="%"
+        />
+        <SummaryItem label="平均BMI" value={averageBmiLabel} />
       </View>
     </Card>
   )
